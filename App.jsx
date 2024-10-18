ca3cbad3a8980f0464d33cba55b50af2f37bae8f
AdityaDey2504 <163118147+AdityaDey2504@users.noreply.github.com>
2024-10-18 06:54:09 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Container, Row, Col, Button } from 'react-bootstrap';
+import { FaDiscord, FaLinkedin, FaGithub, FaAppStore, FaGooglePlay } from 'react-icons/fa';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import Header from './assets/Components/Header/Header'
+
+const App = () => {
+  return (
+    <Header />
+  );
+};
+
+export default App;
